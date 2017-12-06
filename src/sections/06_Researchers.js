a50442485e2cd94c8878763ddeae2925192b425b
egormarushko <nebegan@gmail.com>
2017-12-06 18:07:26 +0300

    replace http to https

--- src/sections/06_Researchers.js
+++ src/sections/06_Researchers.js
@@ -9,7 +9,7 @@ import button from 'components/Button'
 
 import { Root, Container } from './04_Team'
 
-const ph = 'http://via.placeholder.com/350/eee/777?text=Person'
+const ph = 'https://via.placeholder.com/350/eee/777?text=Person'
 
 const lorem = `
 Nullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.
